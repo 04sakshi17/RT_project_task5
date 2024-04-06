c183150cdee64c1b4798510d56d463e8757a9e26
sakshi <sakshimaurya017@gmail.com>
2024-04-06 14:23:21 +0530

    add intial files

---
+++ src/pages/ListDetail.js
@@ -0,0 +1,119 @@
+// import { useContext ,useEffect } from 'react';
+// import styled from 'styled-components';
+// import { useNavigate, useParams } from 'react-router-dom';
+// //import useDataFetching from '../hooks/useDataFetching';
+// import NavBar from '../components/NavBar/NavBar';
+// import ListItem from '../components/ListItem/ListItem';
+// import ListsContext from '../context/ListsContext';
+// import ItemsContext from '../context/ItemsContext';
+// const ListItemWrapper = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   flex-direction: column;
+//   margin: 2% 5%;
+// `;
+
+// function ListDetail() {
+
+//   let navigate = useNavigate();
+//   const { listId } = useParams();
+
+//   // const [loading, error, data] = useDataFetching(
+//   //   'https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/items/',
+//   // );
+
+// //   const { loading, error, items: data } = 
+// //  useContext(ItemsContext);
+// const { loading, error, items, fetchItems } = 
+//  useContext(ItemsContext);
+//  const { list, fetchList } = 
+//  useContext(ListsContext);
+//   // const [items, setItems] = useState([]);
+
+//   // useEffect(() => {
+//   //   data &&
+//   //     listId &&
+//   //     setItems(data.filter((item) => item.listId === parseInt(listId)));
+//   // }, [data, listId]);
+//   useEffect(() => {
+//     listId && !items.length && fetchItems(listId);
+//      }, [fetchItems, items, listId]);
+
+
+//      useEffect(() => {
+//       listId && fetchList(listId);
+//        }, [fetchList, listId]);
+//   return (
+//     <>
+//       {navigate && (
+//         <NavBar
+//           goBack={() => navigate(-1)}
+//           openForm={() => navigate(`/list/${listId}//new`)}
+//           title={list && list.title}
+//         />
+//       )}
+//       <ListItemWrapper>
+//         {loading || error ? (
+//           <span>{error || 'Loading...'}</span>
+//         ) : (
+//           items.map((item) => <ListItem key={item.id} data={item} />)
+//         )}
+//       </ListItemWrapper>
+//     </>
+//   );
+// }
+
+// export default ListDetail;
+
+
+import { useEffect, useContext } from 'react';
+import styled from 'styled-components';
+import { useNavigate, useParams } from 'react-router-dom';
+import ItemsContext from '../context/ItemsContext';
+import ListsContext from '../context/ListsContext';
+import NavBar from '../components/NavBar/NavBar';
+import ListItem from '../components/ListItem/ListItem';
+
+const ListItemWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 2% 5%;
+`;
+
+function ListDetail() {
+  let navigate = useNavigate();
+  const { listId } = useParams();
+
+  const { loading, error, items, fetchItems } = useContext(ItemsContext);
+  const { list, fetchList } = useContext(ListsContext);
+
+  useEffect(() => {
+    listId && !items.length && fetchItems(listId);
+  }, [fetchItems, items, listId]);
+
+  useEffect(() => {
+    listId && fetchList(listId);
+  }, [fetchList, listId]);
+
+  return (
+    <>
+      {navigate && (
+        <NavBar
+          goBack={() => navigate(-1)}
+          openForm={() => navigate(`/list/${listId}/new`)}
+          title={list && list.title}
+        />
+      )}
+      <ListItemWrapper>
+        {loading || error ? (
+          <span>{error || 'Loading...'}</span>
+        ) : (
+          items.map((item) => <ListItem key={item.id} data={item} />)
+        )}
+      </ListItemWrapper>
+    </>
+  );
+}
+
+export default ListDetail;
